6fb7b78ed1dfad195eafddcf6c4f43a8841b3b08
Yosep Kim <kwestmin@gmail.com>
2024-02-21 17:38:45 +0900

    comment 놀라운 문자열 20240221 김요셉

--- 놀라운 문자열.py
+++ 놀라운 문자열.py
@@ -1,13 +1,15 @@
 import sys
 
 
+# 놀라운(surprising) 문자열인지 아닌지 확인 후 ans에 저장하는 함수
 def sol():
-    if (n := len(string)) < 3:
+    if (n := len(string)) < 3:  # 철자 수가 2개 이하는 놀라운 문자열임
         ans.append(string + surprising)
     else:
         for i in range(n - 1):
             tmp = set()
             for j in range(n - i - 1):
+                # 집합을 통해서 D쌍의 유일성 확인
                 if (k := string[j] + string[j + i + 1]) in tmp:
                     ans.append(string + not_surprising)
                     return
@@ -16,8 +18,11 @@ def sol():
         ans.append(string + surprising)
 
 
+# 초기 세팅
 input, ans = sys.stdin.readline, []
 surprising, not_surprising = " is surprising.", " is NOT surprising."
+
+# '*'이 입력되기 전까지 계속 실행
 while (string := input().strip()) != "*":
     sol()
-print("\n".join(ans))
+print("\n".join(ans))  # 정답을 줄바꿈하며 출력
