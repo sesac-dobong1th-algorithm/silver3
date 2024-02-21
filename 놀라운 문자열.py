08c5c93ec1d65007f3b48cea9c32633a5b210a44
Yosep Kim <kwestmin@gmail.com>
2024-02-21 09:54:58 +0900

    add 놀라운 문자열 20240221

---
+++ 놀라운 문자열.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def sol():
+    if (n := len(string)) < 3:
+        ans.append(string + surprising)
+    else:
+        for i in range(n - 1):
+            tmp = set()
+            for j in range(n - i - 1):
+                if (k := string[j] + string[j + i + 1]) in tmp:
+                    ans.append(string + not_surprising)
+                    return
+                else:
+                    tmp.add(k)
+        ans.append(string + surprising)
+
+
+input, ans = sys.stdin.readline, []
+surprising, not_surprising = " is surprising.", " is NOT surprising."
+while (string := input().strip()) != "*":
+    sol()
+print("\n".join(ans))
