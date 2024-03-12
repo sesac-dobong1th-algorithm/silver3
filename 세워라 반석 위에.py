5b204bbfd7cfb09c404f19b37b4697f0519191b4
Yosep Kim <kwestmin@gmail.com>
2024-03-12 12:57:49 +0900

    fix 세워라 반석 위에.py 김요셉

--- 세워라 반석 위에.py
+++ 세워라 반석 위에.py
@@ -1,87 +1,35 @@
-# import sys
-# from collections import deque
-
-
-# def check(num):
-#     li = [i for i, j in enumerate(num) if j]
-#     return li[-1] - li[0] <= 2
-
-
-# def main():
-#     input = sys.stdin.readline
-#     input()
-#     num = [0] * 11
-#     q = deque([])
-#     ans = 1
-#     for a in map(int, input().split()):
-#         q.append(a)
-#         num[a] += 1
-#         while not check(num):
-#             num[q.popleft()] -= 1
-#         if ans < (t := len(q)):
-#             ans = t
-#     print(ans)
-
-
-# main()
-
-
 import sys
 
 
 def main():
     input = sys.stdin.readline
+
+    # 부분 수열에 포함되는 숫자의 존재 여부(갯수)를 확일할 리스트
     nums = [0] * 11
+
+    # s: 부분 수열의 첫 원소의 원래 인덱스
+    # ans, tmp: 부분 수열의 길이(정답, 현재)
     s = ans = tmp = 0
     input()
+
     arr = list(map(int, input().split()))
     for a in arr:
         nums[a] += 1
         tmp += 1
+
+        # li: 부분 수열에 존재하는 숫자만 모아둔 리스트
         li = [i for i, j in enumerate(nums) if j]
+
+        # li는 오름차순이라서 처음과 마지막 원소가 최솟값, 최댓값임
         if li[-1] - li[0] > 2:
             nums[arr[s]] -= 1
             tmp -= 1
             s += 1
+
+        # 가능한 부분 수열 중 길이를 최댓값 업데이트
         if ans < tmp:
             ans = tmp
     print(ans)
 
 
 main()
-
-
-# import sys
-# def main():
-#     input = sys.stdin.readline
-#     n = int(input())
-#     arr = list(map(int, input().split()))
-#     ans = 0
-#     for s in range(n - 1):
-#         M = m = arr[s]
-#         for e in range(s + 1, n):
-#             if M < (t := arr[e]):
-#                 M = t
-#             elif m > t:
-#                 m = t
-#             if M - m > 2:
-#                 break
-#             if ans < (l := e - s + 1):
-#                 ans = l
-#     print(ans)
-# main()
-
-
-# import sys
-# def main():
-#     input = sys.stdin.readline
-#     n = int(input())
-#     arr = list(map(int, input().split()))
-#     for length in range(n, 0, -1):
-#         for start_idx in range(n - length + 1):
-#             tmp = set(arr[start_idx : start_idx + length])
-#             M, m = max(tmp), min(tmp)
-#             if M - m <= 2:
-#                 print(length)
-#                 return
-# main()
